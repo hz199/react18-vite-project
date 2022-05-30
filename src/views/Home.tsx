e3f195137c8fba17839a836fb9c3416b40d841ae
朱贺 <zhuhe36784@hundsun.com>
2022-05-30 19:36:37 +0800

    fix:

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,11 @@
 import { useAppDispatch, useAppSelector } from '../hooks/store'
 import { decrement, increment } from '../stores/modules/user'
 import { Button } from 'antd'
-import { useQuery } from 'react-query'
-import { getUserInfo } from '../server/user'
 import { Link } from 'react-router-dom'
 
 function Home() {
   const count = useAppSelector((state) => state.user.value)
   const dispatch = useAppDispatch()
-
-  const { isLoading, status } = useQuery('user', () => getUserInfo())
-  console.log(isLoading, status)
 
   return (
     <div>
