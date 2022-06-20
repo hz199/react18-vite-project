650739f2d57d0723d122fb79b3e699f9124d3b29
朱贺 <zhuhe36784@hundsun.com>
2022-06-20 13:32:16 +0800

    feat: pwa插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,44 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import vitePluginImp from 'vite-plugin-imp'
 import viteMockPlugin from './plugins/vitePluginMock'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'PWA Router',
+    short_name: 'PWA Router',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +52,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    VitePWA(pwaOptions),
     tsconfigPaths(),
     vitePluginImp({
       libList: [
